4fd31f60ff9b99e18b92ae6486576d7fc6b71dd5
yonicb <yonicb20@gmail.com>
2020-01-01 10:17:47 -0500

    perf: ⚡️ Update v1.0.0

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,7 @@
-var hello=function(l){console.log("Hello "+l)};exports=hello;
+/*!
+ * Cregex v1.0.1 (https://github.com/yonicb/cregex)
+ * Copyright 2019 The Cregex Authors
+ * Copyright 2019 Yoni Calsin <@helloyonicb@gmail.com>.
+ * Licensed under MIT (https://github.com/yonicb/cregex/blob/master/LICENSE)
+ */
+var hello = function (l) { console.log("Hello " + l) }; exports = hello;
