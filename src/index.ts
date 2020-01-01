db44113e60729594aa6a1b3d1b6774d877fd9aa2
yonicb <yonicb20@gmail.com>
2020-01-01 10:23:35 -0500

    perf: ⚡️ Update v1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*!
- * Cregex v1.0.1 (https://github.com/yonicb/cregex)
+ * Cregex v1.0.2 (https://github.com/yonicb/cregex)
  * Copyright 2019 The Cregex Authors
  * Copyright 2019 Yoni Calsin <@helloyonicb@gmail.com>.
  * Licensed under MIT (https://github.com/yonicb/cregex/blob/master/LICENSE)
@@ -7,4 +7,4 @@
 const hello = (name: string) => {
     console.log(`Hello ${name}`);
 }
-exports = hello;
+module.exports = hello;
