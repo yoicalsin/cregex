e16f9437e8c48acaf83fa5a61f6a72059171badc
yonicb <yonicb20@gmail.com>
2020-01-01 09:58:39 -0500

    perf: ⚡️ Remove map and add header comment

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
+/*!
+ * Cregex v1.0.0 (https://github.com/yonicb/cregex)
+ * Copyright 2019 The Cregex Authors
+ * Copyright 2019 Yoni Calsin <@helloyonicb@gmail.com>.
+ * Licensed under MIT (https://github.com/yonicb/cregex/blob/master/LICENSE)
+ */
 var hello=function(l){console.log("Hello "+l)};
-//# sourceMappingURL=index.js.map
